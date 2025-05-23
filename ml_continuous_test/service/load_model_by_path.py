bd6ee67b6fe32ba38b34f1be2d9a65c2fb467cfb
iuryrosal <iuryrosal@gmail.com>
2025-05-22 22:27:49 -0300

    ✨ feat: add LoadModelByPath

---
+++ ml_continuous_test/service/load_model_by_path.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+import pickle
+from sklearn.base import ClassifierMixin, RegressorMixin
+from tensorflow import keras
+
+from model.ml_model import MLModel, ModelTechnology, ModelType
+
+
+class LoadModelByPath:
+    def __init__(self, models_trained) -> None:
+        self.models_path = models_trained
+    
+    def load_all_models(self):
+        all_models = []
+        for model_path in self.models_path:
+            pathlib_obj_with_model = Path(model_path)
+            all_models += self.load_sklearn_model(pathlib_obj_with_model)
+            all_models += self.load_tensorflow_model(pathlib_obj_with_model)
+        return all_models
+
+    def load_sklearn_model(self, pathlib_obj):
+        models = []
+        list_of_models_path = list(pathlib_obj.glob("**/*.obj")) + list(pathlib_obj.glob("**/*.pkl"))
+        for model_path in list_of_models_path:
+            with open(model_path, 'rb') as fp:
+                model_loaded = pickle.load(fp)
+                model_type = None
+                if isinstance(model_loaded, ClassifierMixin):
+                    model_type = ModelType.classifier.value
+                elif isinstance(model_loaded, RegressorMixin):
+                    model_type = ModelType.regressor.value
+                else:
+                    raise ValueError(
+                        f"Model have invalid type. Current model type: {type(model_loaded)}"
+                    )
+                models.append(
+                    MLModel(
+                        model_object=model_loaded,
+                        model_technology=ModelTechnology.sklearn.value,
+                        model_type=model_type
+                    )
+                )
+        return models
+
+    def load_tensorflow_model(self, pathlib_obj):
+        models = []
+        list_of_models_path = list(pathlib_obj.glob("**/*.h5"))
+        for model_path in list_of_models_path:
+            model_loaded = keras.models.load_model(model_path)
+            loss = model_loaded.loss
+            activation = model_loaded.layers[-1].activation.__name__
+
+            model_type = None
+            if 'categorical_crossentropy' in loss or 'binary_crossentropy' in loss:
+                model_type = ModelType.classifier.value
+            elif 'mse' in loss or 'mae' in loss:
+                model_type = ModelType.regressor.value
+            elif activation in ['softmax', 'sigmoid']:
+                model_type = ModelType.classifier.value
+            elif activation == 'linear':
+                model_type = ModelType.regressor.value
+            else:
+                raise ValueError(
+                    f"Model have invalid type. Current model type: {type(model_loaded)}"
+                )
+            models.append(
+                MLModel(
+                    model_object=model_loaded,
+                    model_technology=ModelTechnology.tensorflow.value,
+                    model_type=model_type
+                )
+            )
+        return models
