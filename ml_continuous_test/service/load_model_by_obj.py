48225d8d3bc02e2c131b09a6138c78b68e8b8bad
iuryrosal <iuryrosal@gmail.com>
2025-05-22 22:27:28 -0300

    ✨ feat: add LoadModelByObject

---
+++ ml_continuous_test/service/load_model_by_obj.py
@@ -0,0 +1,61 @@
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
+from tensorflow.keras.models import Model, Sequential
+
+from model.ml_model import MLModel, ModelTechnology, ModelType
+
+
+class LoadModelByObject:
+    def __init__(self, models_trained) -> None:
+        self.models_obj = models_trained
+    
+    def load_all_models(self):
+        all_models = []
+        for model_obj in self.models_obj:
+            if isinstance(model_obj, BaseEstimator):
+                all_models.append(self.load_sklearn_model(model_obj))
+            elif isinstance(model_obj, (Model, Sequential)):
+                all_models.append(self.load_tensorflow_model(model_obj))
+            else:
+                raise ValueError(
+                    "Invalid Model Technology."
+                )
+        return all_models
+
+    def load_sklearn_model(self, model_obj):
+        model_type = None
+        if isinstance(model_obj, ClassifierMixin):
+            model_type = ModelType.classifier.value
+        elif isinstance(model_obj, RegressorMixin):
+            model_type = ModelType.regressor.value
+        else:
+            raise ValueError(
+                f"Model have invalid type. Current model type: {type(model_obj)}"
+            )
+        return MLModel(
+            model_object=model_obj,
+            model_technology=ModelTechnology.sklearn.value,
+            model_type=model_type
+        )
+
+    def load_tensorflow_modeL(self, model_obj):
+        loss = model_obj.loss
+        activation = model_obj.layers[-1].activation.__name__
+
+        model_type = None
+        if 'categorical_crossentropy' in loss or 'binary_crossentropy' in loss:
+            model_type = ModelType.classifier.value
+        elif 'mse' in loss or 'mae' in loss:
+            model_type = ModelType.regressor.value
+        elif activation in ['softmax', 'sigmoid']:
+            model_type = ModelType.classifier.value
+        elif activation == 'linear':
+            model_type = ModelType.regressor.value
+        else:
+            raise ValueError(
+                f"Model have invalid type. Current model type: {type(model_obj)}"
+            )
+        return MLModel(
+            model_object=model_obj,
+            model_technology=ModelTechnology.tensorflow.value,
+            model_type=model_type
+        )
