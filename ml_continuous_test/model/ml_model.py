6e88bb13c0bf6f104a4851a070c4b01968addc69
iuryrosal <iuryrosal@gmail.com>
2025-05-22 22:27:10 -0300

    ✨ feat: add ml_model

---
+++ ml_continuous_test/model/ml_model.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Union
+from sklearn.base import BaseEstimator
+from tensorflow.keras.models import Model, Sequential
+
+
+class ModelTechnology(str, Enum):
+    sklearn = "sklearn"
+    tensorflow = "tensorflow"
+
+class ModelType(str, Enum):
+    classifier = "classifier"
+    regressor = "regressor"
+
+class MLModel(BaseModel):
+    model_object: Union[BaseEstimator, Model, Sequential]
+    model_technology: ModelTechnology
+    model_type: ModelType
+
+    class Config:
+        arbitrary_types_allowed = True
