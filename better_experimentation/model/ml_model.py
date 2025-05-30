ab45c4959facf28edb42438fb49bd6d4f4f24ec7
iuryrosal <iuryrosal@gmail.com>
2025-05-30 15:38:13 -0300

    📝 docs(ml_model): add docstrings

--- better_experimentation/model/ml_model.py
+++ better_experimentation/model/ml_model.py
@@ -6,14 +6,20 @@ from tensorflow.keras.models import Model, Sequential
 
 
 class ModelTechnology(str, Enum):
+    """Supported Technology Types for Supervised Machine Learning Models
+    """
     sklearn = "sklearn"
     tensorflow = "tensorflow"
 
 class ModelType(str, Enum):
+    """Supported Models Types for Supervised Machine Learning Models
+    """
     classifier = "classifier"
     regressor = "regressor"
 
 class MLModel(BaseModel):
+    """A generic representation of a trained and loaded model
+    """
     model_index: int
     model_name: str
     model_object: Union[BaseEstimator, Model, Sequential]
