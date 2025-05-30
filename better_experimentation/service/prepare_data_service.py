2c670cc41784f7ee8cfe5c251d9ba399b17f8144
iuryrosal <iuryrosal@gmail.com>
2025-05-30 13:36:11 -0300

    fix(prepare_data_service): adjust to use model object

--- better_experimentation/service/prepare_data_service.py
+++ better_experimentation/service/prepare_data_service.py
@@ -13,7 +13,7 @@ class PrepareDataService:
         for score_target in scores_target:
             self.scores[score_target] = {}
             for i, model in enumerate(models):
-                self.scores[score_target][f"{i}"] = []
+                self.scores[score_target][f"{model.model_index}"] = []
 
         self.__kf = KFold(n_splits=n_splits, shuffle=True)
         data_test_split = self.__kf.split(X=X_test)
@@ -22,20 +22,20 @@ class PrepareDataService:
             Y_fold = Y_fold.values.ravel()
             for i, model in enumerate(models):
                 Y_pred = model.model_object.predict(X_fold)
-                self.__collect_metric_result(f"{i}", Y_fold, Y_pred)
+                self.__collect_metric_result(model, Y_fold, Y_pred)
 
     @handle_exceptions(__log_service.get_logger(__name__))
-    def __collect_metric_result(self, model_id, Y_fold, Y_pred):
+    def __collect_metric_result(self, model, Y_fold, Y_pred):
         """Collects metrics for the given model and test data."""
         for score_target in self.scores.keys():
             if score_target == "accuracy":
-                self.scores[score_target][model_id].append(accuracy_score(Y_fold, Y_pred))
+                self.scores[score_target][str(model.model_index)].append(accuracy_score(Y_fold, Y_pred))
             elif score_target == "f1":
-                self.scores[score_target][model_id].append(f1_score(Y_fold, Y_pred))
+                self.scores[score_target][str(model.model_index)].append(f1_score(Y_fold, Y_pred))
             elif score_target == "precision":
-                self.scores[score_target][model_id].append(precision_score(Y_fold, Y_pred))
+                self.scores[score_target][str(model.model_index)].append(precision_score(Y_fold, Y_pred))
             elif score_target == "recall":
-                self.scores[score_target][model_id].append(recall_score(Y_fold, Y_pred))
+                self.scores[score_target][str(model.model_index)].append(recall_score(Y_fold, Y_pred))
 
     @handle_exceptions(__log_service.get_logger(__name__))
     def get_scores_data(self):
