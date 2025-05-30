1e9164a9a467172b8c5facd674a71b0050fcfccd
iuryrosal <iuryrosal@gmail.com>
2025-05-30 15:38:06 -0300

    📝 docs(report): add docstrings

--- better_experimentation/model/report.py
+++ better_experimentation/model/report.py
@@ -5,6 +5,8 @@ import datetime
 from better_experimentation.model.ab_test_results import ShapiroWilkTestResult, LeveneTestResult, BartlettTestResult, AnovaTestResult, TStudentTestResult, TurkeyTestResult, KruskalWallisTestResult, MannWhitneyTestResult
 
 class ScoreDescribed(BaseModel):
+    """Generates a statistical summary of the data distribution of a performance metric around a ML model
+    """
     model_index: str
     mean: float = None
     std: float = None
@@ -14,7 +16,8 @@ class ScoreDescribed(BaseModel):
     mode: float = None
 
 class ABTestReport(BaseModel):
-    """Represents the result of the application of statistical tests to validate the significance of the models."""
+    """Represents the result of the application of statistical tests to validate the significance of the models, involving performance data after testing
+    """
     pipeline_track: list[str] = []
     shapirowilk: list[ShapiroWilkTestResult] = []
     levene: list[LeveneTestResult] = []
@@ -26,12 +29,15 @@ class ABTestReport(BaseModel):
     tstudent: TStudentTestResult = None
 
 class GeneralReportByScore(BaseModel):
-    """It groups together all relevant information about statistics and comparison of model results with statistical tests."""
+    """Groups all relevant information about statistics and comparison of model results with statistical tests around a given performance metric
+    """
     score_target: str
     score_described: list[ScoreDescribed] = []
     ab_tests: ABTestReport = None
 
 class GeneralReport(BaseModel):
+    """The General Report that aggregates all the details of the statistical test results for the specified performance metrics
+    """
     reports_by_score: list[GeneralReportByScore] = []
     better_model_by_score: list[str] = []
     best_model_index: Union[int, None] = None
