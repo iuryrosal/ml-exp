2f9da890e75f3375c568b82149981de6a4c8d8bd
iuryrosal <iuryrosal@gmail.com>
2025-06-07 14:39:05 -0300

    📝 docs(tensorflow_model_repository): add docstring

--- better_experimentation/repository/tensorflow_model_repository.py
+++ better_experimentation/repository/tensorflow_model_repository.py
@@ -7,10 +7,27 @@ from better_experimentation.model.ml_model import MLModel, ModelTechnology, Mode
 
 
 class TensorflowModelRepository(IModelRepository):
+    """Repository to handle loading models (via object and file path) in the context of Sklearn dependency
+
+    Args:
+        IModelRepository (ABC): Interface for repositories responsible for loading machine learning models
+    """
     def __init__(self):
         super().__init__()
 
-    def load_model_by_obj(self, model_idx, model_obj):
+    def load_model_by_obj(self, model_idx: int, model_obj) -> MLModel:
+        """Loads models from the instantiated object
+
+        Args:
+            model_idx (int): Model incidence for identification
+            model_obj (BaseEstimator): Object that is parked the trained model to be loaded
+
+        Raises:
+            ValueError: If the instance type is not within the mapping
+
+        Returns:
+            MLModel: Model loaded and processed to be used in the testing phases
+        """
         loss = model_obj.loss
         activation = model_obj.layers[-1].activation.__name__
 
@@ -35,7 +52,18 @@ class TensorflowModelRepository(IModelRepository):
             model_type=model_type
         )
 
-    def load_model_by_path(self, pathlib_obj):
+    def load_model_by_path(self, pathlib_obj: Path) -> list[MLModel]:
+        """Loads models from the path that model is stored
+
+        Args:
+            pathlib_obj (Path): Base path where stored models exist loaded in PathLib
+
+        Raises:
+            ValueError: If the instance type is not within the mapping
+
+        Returns:
+            list[MLModel]: List of models loaded and processed to be used in the testing phases
+        """
         models = []
         list_of_models_path = list(pathlib_obj.glob("**/*.obj")) + list(pathlib_obj.glob("**/*.pkl"))
         for model_idx, model_path in enumerate(list_of_models_path):
