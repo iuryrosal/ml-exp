2dded945ed5a066534349f0bdb5e84d397bfa1f5
iuryrosal <iuryrosal@gmail.com>
2025-07-28 22:33:05 -0300

    fix(load_model_service): add model_name usage

--- better_experimentation/service/load_model_service.py
+++ better_experimentation/service/load_model_service.py
@@ -6,9 +6,11 @@ class LoadModelService:
     def __init__(self, model_repository: IModelRepository) -> None:
         self.model_repository = model_repository
 
-    def load_model_by_obj(self, model_idx, model_obj):
-        return self.model_repository.load_model_by_obj(model_idx, model_obj)
-    
-    def load_model_by_path(self, model_path):
+    def load_model_by_obj(self, model_name: str, model_obj):
+        return self.model_repository.load_model_by_obj(model_name=model_name,
+                                                       model_obj=model_obj)
+
+    def load_model_by_path(self, model_path: str, model_name: str) -> MLModel:
         pathlib_obj_with_model = Path(model_path)
-        return self.model_repository.load_model_by_path(pathlib_obj_with_model)
+        return self.model_repository.load_model_by_path(pathlib_obj=pathlib_obj_with_model,
+                                                        model_name=model_name)
